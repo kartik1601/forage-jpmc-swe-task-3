6ceab408f8f989217dd9d9bdad73c92264b540cb
kartik1601 <kartiku904@gmail.com>
2023-12-26 02:19:47 +0530

    Task 03 JPMC
    -Changed the data types fetching from the test file. This includes the the stock prices of the two stocks, their ratio, upper and lower bound for ratio and trigger alerts.
    -Updated the table contents and view to show {ratio, upperBound, lowerBound, triggerAlerts} in the graph 's y-axis along with unique timestamps in the x-axis.
    -Updated the data manipulator to handle the new schema and to display correct prices and ratio, with ±10% range for triggering alerts for stock to buy/sell.

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -1,20 +1,31 @@
 import { ServerRespond } from './DataStreamer';
 
 export interface Row {
-  stock: string,
-  top_ask_price: number,
+  price_abc: Number,
+  price_def: Number,
   timestamp: Date,
+  upper_bound: Number,
+  lower_bound: Number,
+  ratio: Number,
+  trigger_alert: Number | undefined,
 }
 
 
 export class DataManipulator {
-  static generateRow(serverResponds: ServerRespond[]) {
-    return serverResponds.map((el: any) => {
+  static generateRow(serverResponds: ServerRespond[]): Row {
+      const price_abc = (serverResponds[0].top_ask.price + serverResponds[0].top_bid.price)/2;
+      const price_def = (serverResponds[1].top_ask.price + serverResponds[1].top_bid.price)/2;
+      const ratio = price_abc/price_def;
+      const upper_bound = 1.1;
+      const lower_bound = 0.9;
       return {
-        stock: el.stock,
-        top_ask_price: el.top_ask && el.top_ask.price || 0,
-        timestamp: el.timestamp,
+        price_abc,
+        price_def,
+        ratio,
+        timestamp: serverResponds[0].timestamp > serverResponds[1].timestamp ? serverResponds[0].timestamp : serverResponds[1].timestamp,
+        upper_bound,
+        lower_bound,
+        trigger_alert: (ratio > upper_bound || ratio < lower_bound) ? ratio : undefined,
       };
-    })
   }
 }
